a89df1134376cfb1627759b2c519f3fefa30e4a8
Pradeep Boopathy <77095302+PradeepBoopathy@users.noreply.github.com>
2024-07-25 19:06:59 +0530

    Add files via upload

---
+++ util/joint_util.py
@@ -0,0 +1,111 @@
+import numpy as np
+
+
+def get_max_preds(batch_heatmaps):
+    assert isinstance(batch_heatmaps, np.ndarray), \
+        'batch_heatmaps should be numpy.ndarray'
+    assert batch_heatmaps.ndim == 4, 'batch_images should be 4-ndim'
+
+    batch_size = batch_heatmaps.shape[0]
+    num_joints = batch_heatmaps.shape[1]
+    width = batch_heatmaps.shape[3]
+    heatmaps_reshaped = batch_heatmaps.reshape((batch_size, num_joints, -1))
+    idx = np.argmax(heatmaps_reshaped, 2)
+    maxvals = np.amax(heatmaps_reshaped, 2)
+
+    maxvals = maxvals.reshape((batch_size, num_joints, 1))
+    idx = idx.reshape((batch_size, num_joints, 1))
+
+    preds = np.tile(idx, (1, 1, 2)).astype(np.float32)
+
+    preds[:, :, 0] = (preds[:, :, 0]) % width
+    preds[:, :, 1] = np.floor((preds[:, :, 1]) / width)
+
+    pred_mask = np.tile(np.greater(maxvals, 0.0), (1, 1, 2))
+    pred_mask = pred_mask.astype(np.float32)
+
+    preds *= pred_mask
+    return preds, maxvals
+
+
+def get_avg_preds(batch_heatmaps, threshold=0.3):
+    preds = []
+    maxvals = []
+    for heatmaps in batch_heatmaps:
+        heatmap_preds = []
+        heatmap_maxvals = []
+        for heatmap in heatmaps:
+            valid_elements = np.array(np.where(heatmap > threshold)).T
+            if len(valid_elements) == 0:
+                heatmap_preds.append([0.0, 0.0])
+                heatmap_maxvals.append([0])
+                continue
+
+            weights = []
+            for valid_element in valid_elements:
+                weights.append(heatmap[valid_element[0]][valid_element[1]])
+            heatmap_maxval = max(weights)
+            weights = np.array(weights) / sum(weights)
+
+            pred_x = 0
+            pred_y = 0
+            for valid_element, weight in zip(valid_elements, weights):
+                pred_x += weight * valid_element[1]
+                pred_y += weight * valid_element[0]
+
+            heatmap_preds.append([pred_x, pred_y])
+            heatmap_maxvals.append([heatmap_maxval])
+        preds.append(heatmap_preds)
+        maxvals.append(heatmap_maxvals)
+    return np.array(preds), np.array(maxvals)
+
+
+def calc_dists(preds, target, normalize):
+    preds = preds.astype(np.float32)
+    target = target.astype(np.float32)
+    dists = np.zeros((preds.shape[1], preds.shape[0]))
+    for n in range(preds.shape[0]):
+        for c in range(preds.shape[1]):
+            if target[n, c, 0] > 1 and target[n, c, 1] > 1:
+                normed_preds = preds[n, c, :] / normalize[n]
+                normed_targets = target[n, c, :] / normalize[n]
+                dists[c, n] = np.linalg.norm(normed_preds - normed_targets)
+            else:
+                dists[c, n] = -1
+    return dists
+
+
+def dist_acc(dists, thr=0.5):
+    dist_cal = np.not_equal(dists, -1)
+    num_dist_cal = dist_cal.sum()
+    if num_dist_cal > 0:
+        return np.less(dists[dist_cal], thr).sum() * 1.0 / num_dist_cal
+    else:
+        return -1
+
+
+def accuracy(output, target, hm_type='gaussian', thr=0.5):
+    idx = list(range(output.shape[1]))
+    norm = 1.0
+    if hm_type == 'gaussian':
+        pred, _ = get_max_preds(output)
+        target, _ = get_max_preds(target)
+        h = output.shape[2]
+        w = output.shape[3]
+        norm = np.ones((pred.shape[0], 2)) * np.array([h, w]) / 10
+    dists = calc_dists(pred, target, norm)
+
+    acc = np.zeros((len(idx) + 1))
+    avg_acc = 0
+    cnt = 0
+
+    for i in range(len(idx)):
+        acc[i + 1] = dist_acc(dists[idx[i]])
+        if acc[i + 1] >= 0:
+            avg_acc = avg_acc + acc[i + 1]
+            cnt += 1
+
+    avg_acc = avg_acc / cnt if cnt != 0 else 0
+    if cnt != 0:
+        acc[0] = avg_acc
+    return acc, avg_acc, cnt, pred
